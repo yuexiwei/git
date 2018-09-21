7c8a0cecc49b62cd8793f527610fd1fd30e044d6
Matthew DeVore <matvore@google.com>
2018-09-21 13:32:04 -0700

    list-objects-filter: implement filter tree:0
    Teach list-objects the "tree:0" filter which allows for filtering
    out all tree and blob objects (unless other objects are explicitly
    specified by the user). The purpose of this patch is to allow smaller
    partial clones.
    
    The name of this filter - tree:0 - does not explicitly specify that
    it also filters out all blobs, but this should not cause much confusion
    because blobs are not at all useful without the trees that refer to
    them.
    
    I also considered only:commits as a name, but this is inaccurate because
    it suggests that annotated tags are omitted, but actually they are
    included.
    
    The name "tree:0" allows later filtering based on depth, i.e. "tree:1"
    would filter out all but the root tree and blobs. In order to avoid
    confusion between 0 and capital O, the documentation was worded in a
    somewhat round-about way that also hints at this future improvement to
    the feature.
    
    Signed-off-by: Matthew DeVore <matvore@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t6112-rev-list-filters-objects.sh
+++ t/t6112-rev-list-filters-objects.sh
@@ -225,6 +225,19 @@ test_expect_success 'rev-list W/ --missing=print and --missing=allow-any for tre
 	test_must_be_empty rev_list_err
 '
 
+# Test tree:0 filter.
+
+test_expect_success 'verify tree:0 includes trees in "filtered" output' '
+	git -C r3 rev-list HEAD --quiet --objects --filter-print-omitted --filter=tree:0 |
+	awk -f print_1.awk |
+	sed s/~// |
+	xargs -n1 git -C r3 cat-file -t |
+	sort -u >filtered_types &&
+
+	printf "blob\ntree\n" >expected &&
+	test_cmp expected filtered_types
+'
+
 # Delete some loose objects and use rev-list, but WITHOUT any filtering.
 # This models previously omitted objects that we did not receive.
 
